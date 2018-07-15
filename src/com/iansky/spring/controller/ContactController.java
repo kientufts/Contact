5ed752778b3c14e57643968936f7ef75dc2eda63
Kientufts <letrungkien1991@gmail.com>
2018-07-14 19:40:54 -0700

    inject contactDAO into ContactController

--- src/com/iansky/spring/controller/ContactController.java
+++ src/com/iansky/spring/controller/ContactController.java
@@ -1,15 +1,27 @@
 package com.iansky.spring.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.iansky.spring.dao.ContactDAO;
+import com.iansky.spring.entity.Contact;
 
 @Controller
 @RequestMapping("/contact")
 public class ContactController {
 	
+	@Autowired
+	private ContactDAO contactDao;
+	
 	@RequestMapping("/list")
 	public String listContacts(Model model) {
+		
+		List<Contact> contacts = contactDao.getContacts();
+		model.addAttribute("contacts", contacts);
 		return "list-contacts";
 	}
 
