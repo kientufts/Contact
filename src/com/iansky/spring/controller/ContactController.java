05e26e7c46c98628815724a3c940afa201c12d31
Kientufts <letrungkien1991@gmail.com>
2018-07-14 20:28:23 -0700

    change to @GetMapping annotation

--- src/com/iansky/spring/controller/ContactController.java
+++ src/com/iansky/spring/controller/ContactController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.iansky.spring.dao.ContactDAO;
@@ -17,7 +18,7 @@ public class ContactController {
 	@Autowired
 	private ContactDAO contactDao;
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listContacts(Model model) {
 		
 		List<Contact> contacts = contactDao.getContacts();
